2eea299e86e71e205545bd9e3757e3df24f9a4a0
MacaronFR <denisft77@gmail.com>
2025-05-21 11:13:32 +0200

    Fix auth reload problem

--- src/Hooks/Authentication/AuthenticationProvider.tsx
+++ src/Hooks/Authentication/AuthenticationProvider.tsx
@@ -1,24 +1,171 @@
-import {createContext} from "react";
+import {createContext, useCallback, useEffect, useState} from "react";
+import pkceChallenge from "pkce-challenge"
 import * as React from "react";
+import {useLocation} from "react-router";
+import {SetState} from "../../utils";
 
 interface AuthenticationContextType {
 	clientId: string,
 	baseURL: string,
-	tokenUrl: string
+	tokenUrl: string,
+	loading: boolean,
+	setLoading: SetState<boolean>,
+	isFetching: boolean,
+	setIsFetching: SetState<boolean>,
+	login: (destination: string, origin?: string) => Promise<void>,
+	fetchToken: (type: "authorization" | "refresh", code: string) => Promise<void>,
+	token: string | null,
+	logout: (to: string) => void,
+	isAuthenticated: boolean,
 }
 
 export const AuthenticationContext = createContext<AuthenticationContextType>({
 	clientId: "",
 	baseURL: "",
 	tokenUrl: "",
+	loading: true,
+	setLoading: () => {},
+	isFetching: false,
+	setIsFetching: () => {},
+	login: () => Promise.resolve(),
+	fetchToken: () => Promise.resolve(),
+	token: null,
+	logout: () => {},
+	isAuthenticated: false,
 });
 
-interface AuthenticationProviderProps extends AuthenticationContextType{
+interface AuthenticationProviderProps {
+	clientId: string,
+	baseURL: string,
+	tokenUrl: string,
 	children: React.ReactNode;
 }
 
 export function AuthenticationProvider(props: AuthenticationProviderProps) {
-	return <AuthenticationContext.Provider value={props}>
+	const [loading, setLoading] = useState<boolean>(true);
+	const [isFetching, setIsFetching] = useState<boolean>(false);
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+	const location = useLocation();
+
+	const fetchToken = useCallback(async (type: "authorization" | "refresh", code: string) => {
+		setLoading(true);
+		setIsFetching(true);
+		if (type === "authorization") {
+			const codeVerifier = sessionStorage.getItem("code_verifier");
+			if (!codeVerifier) {
+				console.error("No code verifier found");
+				return;
+			}
+
+			try {
+				const tokenResponse = await fetch(props.tokenUrl, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded"
+					},
+					body: new URLSearchParams({
+						grant_type: "authorization_code",
+						code: code,
+						client_id: props.clientId,
+						code_verifier: codeVerifier
+					})
+				});
+
+				const token = await tokenResponse.json();
+
+				localStorage.setItem("token", token.access_token);
+				localStorage.setItem("refresh_token", token.refresh_token);
+				setIsAuthenticated(true);
+				setLoading(false);
+				setIsFetching(false);
+			} catch (e) {
+				console.error(e);
+			}
+		} else if(type === "refresh") {
+			try {
+				const tokenResponse = await fetch(props.tokenUrl, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded"
+					},
+					body: new URLSearchParams({
+						grant_type: "refresh_token",
+						refresh_token: code,
+						client_id: props.clientId,
+					})
+				});
+
+				const token = await tokenResponse.json();
+
+				localStorage.setItem("token", token.access_token);
+				localStorage.setItem("refresh_token", token.refresh_token);
+				setIsAuthenticated(true);
+				setLoading(false);
+				setIsFetching(false);
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}, [props.tokenUrl, props.clientId]);
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const authCode = urlParams.get('code');
+		console.log("Auth code", authCode);
+		if(authCode) {
+			window.location.href = window.location.origin + location.pathname;
+			if(!isFetching) {
+				console.log("fect")
+				fetchToken("authorization", authCode).then(() => {
+					window.location.href = window.location.origin + location.pathname;
+				});
+			}
+		} else {
+			setLoading(false);
+		}
+	}, [location, isFetching, setLoading, setIsFetching, fetchToken]);
+
+	const login = useCallback(async (destination: string, origin?: string) => {
+		const {code_verifier, code_challenge} = await pkceChallenge();
+		sessionStorage.setItem("code_verifier", code_verifier);
+
+		const authUrl = new URL(props.baseURL + destination);
+		authUrl.searchParams.append('client_id', props.clientId);
+		authUrl.searchParams.append('response_type', 'code');
+		authUrl.searchParams.append('redirect_uri', window.location.origin + (origin ?? "/"));
+		authUrl.searchParams.append('code_challenge', code_challenge);
+
+		window.location.href = authUrl.toString();
+	}, [props.baseURL, props.clientId]);
+
+	const logout = useCallback((to: string) => {
+		setIsAuthenticated(false);
+		localStorage.removeItem("token");
+		localStorage.removeItem("refresh_token");
+		window.location.href = to + (location.search === "" ? "?" : "") + "origin=" + location.pathname;
+	}, [location]);
+
+	useEffect(() => {
+		if(localStorage.getItem("token")) {
+			console.log("ayth")
+			setIsAuthenticated(true);
+		}
+	}, []);
+
+	const contextValue = {
+		...props,
+		loading: loading,
+		setLoading: setLoading,
+		isFetching: isFetching,
+		setIsFetching: setIsFetching,
+		login: login,
+		fetchToken: fetchToken,
+		token: localStorage.getItem("token") ?? "",
+		logout: logout,
+		isAuthenticated: isAuthenticated,
+	}
+	return <AuthenticationContext.Provider value={contextValue}>
 		{props.children}
 	</AuthenticationContext.Provider>
 }
--- src/Hooks/Authentication/useAuthentication.ts
+++ src/Hooks/Authentication/useAuthentication.ts
@@ -1,136 +1,22 @@
-import pkceChallenge from "pkce-challenge"
-import {useCallback, useContext, useEffect, useState} from "react";
+import { useContext } from "react";
 import {AuthenticationContext} from "./AuthenticationProvider";
-import {useLocation} from "react-router";
 
 interface UseAuthentication {
-	login: (destination: string, origin?: string) => Promise<void>;
-	fetchToken: (type: "authorization" | "refresh", code: string) => Promise<void>;
-	token: string | null;
-	loading: boolean;
-	logout: (to: string) => void;
+	login: (destination: string, origin?: string) => Promise<void>,
+	fetchToken: (type: "authorization" | "refresh", code: string) => Promise<void>,
+	loading: boolean,
+	logout: (to: string) => void,
+	isAuthenticated: boolean
 }
 
 export default function useAuthentication(): UseAuthentication {
-	const [token, setToken] = useState<string | null>(null);
-	const [loading, setLoading] = useState<boolean>(true);
-	const [isFetching, setIsFetching] = useState<boolean>(false);
 	const context = useContext(AuthenticationContext);
-	const location = useLocation();
-
-	const fetchToken = useCallback(async (type: "authorization" | "refresh", code: string) => {
-		setLoading(true);
-		if (type === "authorization") {
-			const codeVerifier = sessionStorage.getItem("code_verifier");
-			if (!codeVerifier) {
-				console.error("No code verifier found");
-				return;
-			}
-
-			try {
-				const tokenResponse = await fetch(context.tokenUrl, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded"
-					},
-					body: new URLSearchParams({
-						grant_type: "authorization_code",
-						code: code,
-						client_id: context.clientId,
-						code_verifier: codeVerifier
-					})
-				});
-
-				const token = await tokenResponse.json();
-
-				localStorage.setItem("token", token.access_token);
-				localStorage.setItem("refresh_token", token.refresh_token);
-				setToken(token.access_token);
-			} catch (e) {
-				console.error(e);
-				setToken(null);
-				localStorage.removeItem("token");
-				localStorage.removeItem("refresh_token");
-			}
-		} else if(type === "refresh") {
-			try {
-				const tokenResponse = await fetch(context.tokenUrl, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded"
-					},
-					body: new URLSearchParams({
-						grant_type: "refresh_token",
-						refresh_token: code,
-						client_id: context.clientId,
-					})
-				});
-
-				const token = await tokenResponse.json();
-
-				localStorage.setItem("token", token.access_token);
-				localStorage.setItem("refresh_token", token.refresh_token);
-				setToken(token.access_token);
-			} catch (e) {
-				console.error(e);
-				setToken(null);
-				localStorage.removeItem("token");
-				localStorage.removeItem("refresh_token");
-			}
-		}
-		setLoading(false);
-		setIsFetching(false);
-	}, [context]);
-
-	useEffect(() => {
-		const urlParams = new URLSearchParams(location.search);
-		const authCode = urlParams.get('code');
-		if(authCode) {
-			if(!isFetching) {
-				setIsFetching(true);
-				fetchToken("authorization", authCode).then(() => {
-					setLoading(false);
-					setIsFetching(false);
-					window.location.href = window.location.origin + location.pathname;
-				});
-			}
-		} else {
-			setLoading(false);
-		}
-	}, [location, isFetching]);
-
-	const login = useCallback(async (destination: string, origin?: string) => {
-		const {code_verifier, code_challenge} = await pkceChallenge();
-		sessionStorage.setItem("code_verifier", code_verifier);
-
-		const authUrl = new URL(context.baseURL + destination);
-		authUrl.searchParams.append('client_id', context.clientId);
-		authUrl.searchParams.append('response_type', 'code');
-		authUrl.searchParams.append('redirect_uri', window.location.origin + (origin ?? "/"));
-		authUrl.searchParams.append('code_challenge', code_challenge);
-
-		window.location.href = authUrl.toString();
-	}, [context]);
-
-	const logout = useCallback((to: string) => {
-		localStorage.removeItem("token");
-		localStorage.removeItem("refresh_token");
-		setToken(null);
-		window.location.href = to + (location.search === "" ? "?" : "") + "origin=" + location.pathname;
-	}, [location]);
-
-	useEffect(() => {
-		const token = localStorage.getItem("token") ?? "";
-		if(token) {
-			setToken(token);
-		}
-	}, []);
 
 	return {
-		login: login,
-		fetchToken: fetchToken,
-		token: token,
-		loading: loading,
-		logout: logout
+		login: context.login,
+		fetchToken: context.fetchToken,
+		loading: context.loading,
+		logout: context.logout,
+		isAuthenticated: context.isAuthenticated
 	}
 }
