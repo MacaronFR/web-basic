7845ff0cbc2e0954e93ffe9f601e45bd10433609
MacaronFR <denisft77@gmail.com>
2025-07-03 15:40:27 +0200

    Fix ui 2

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
 
 export default function Header(props: HeaderProps) {
 	return (
-		<div className={"bg-header h-12 flex items-center gap-2 px-2"}>
+		<div className={"bg-header h-12 flex items-center gap-2 px-2 py-1"}>
 			{ props.displayMenu && <button onClick={() => props.displayMenu && props.displayMenu(prev => !prev)} className={"block md:hidden text-2xl cursor-pointer"}><FiMenu/></button> }
 			<h1 className={"text-2xl flex gap-2 items-center"}>{props.title}</h1>
 			<div className={"grow"}/>
