84196a87fe3d5f3089d756e62be26ffcbb896a6f
MacaronFR <denisft77@gmail.com>
2025-07-03 15:23:49 +0200

    Fix scroll in table Sticky Header

--- src/Components/Table/HeaderCell.tsx
+++ src/Components/Table/HeaderCell.tsx
@@ -6,7 +6,7 @@ interface HeaderCellProps {
 	children: React.ReactNode,
 	sortable: boolean,
 	sort: SortOrder,
-	setSort?: (sort: SortOrder) => void
+	setSort?: (sort: SortOrder) => void,
 }
 
 export default function HeaderCell(props: HeaderCellProps) {
@@ -22,11 +22,13 @@ export default function HeaderCell(props: HeaderCellProps) {
 		}
 	}, [props.sortable, props.sort, props.setSort]);
 	return(
-		<div onClick={() => toggleSort()} className={clsx("select-none bg-table-header hover:bg-table-header-hover first:rounded-tl-md last:rounded-tr-md px-2 border-b border-table-border flex justify-between items-center", props.sortable && props.setSort && "cursor-pointer")}>
-			<div className={"text-xl text-nowrap py-1"}>{ props.children }</div>
-			<div>
-				{props.sortable && props.sort === SortOrder.Desc && <span className={"text-2xl"}>&#8595;</span>}
-				{props.sortable && props.sort === SortOrder.Asc && <span className={"text-2xl"}>&#8593;</span>}
+		<div className={"sticky top-0"}>
+			<div onClick={() => toggleSort()} className={clsx("select-none bg-table-header hover:bg-table-header-hover first:rounded-tl-md last:rounded-tr-md px-2 border-b border-table-border flex justify-between items-center", props.sortable && props.setSort && "cursor-pointer")}>
+				<div className={"text-xl text-nowrap py-1"}>{ props.children }</div>
+				<div>
+					{props.sortable && props.sort === SortOrder.Desc && <span className={"text-2xl"}>&#8595;</span>}
+					{props.sortable && props.sort === SortOrder.Asc && <span className={"text-2xl"}>&#8593;</span>}
+				</div>
 			</div>
 		</div>
 	);
--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -7,6 +7,7 @@ import Footer from "./Footer";
 import React from "react";
 import {DefaultLoading} from "./DefaultLoading";
 import {Sort, SortOrder} from "./Sort";
+import {clsx} from "clsx";
 
 export interface TableProps<T extends StringIndexedObject> {
 	header: Header<T>[],
@@ -21,7 +22,8 @@ export interface TableProps<T extends StringIndexedObject> {
 	setSort?: SetState<Sort[]>,
 	loading?: boolean,
 	loadingElement?: React.ReactNode,
-	error?: string
+	error?: string,
+	className?: string,
 }
 
 export default function Table<T extends StringIndexedObject>(props: TableProps<T>) {
@@ -30,26 +32,26 @@ export default function Table<T extends StringIndexedObject>(props: TableProps<T
 		return size.map(v => typeof v === "number" ? v + "px" : v ?? "1fr");
 	}, [props.header]);
 	return (
-		<div className={"border border-table-border rounded-md overflow-hidden grow self-start"}>
-			<div className={"grid overflow-x-auto"} style={{gridTemplateColumns: size.join(" ")}}>
-				<div className={"contents"}>
-					{props.header.map((header, index) => {
-						const sortOrder = header.sortable !== false ? props.sort?.find(v => v.id === header.id)?.order ?? SortOrder.None : SortOrder.None;
-						const setSort = props.setSort && ((sort: SortOrder) => {
-							if(props.setSort === undefined) return;
-							props.setSort(prev => {
-								const index = prev.findIndex(v => v.id === header.id);
-								if(index !== -1) {
-									prev[index].order = sort;
-								} else {
-									prev.push({id: header.id, order: sort});
-								}
-								return [...prev];
-							})
-						});
-						return <HeaderCell key={index} sortable={header.sortable ?? true} sort={sortOrder} setSort={setSort}>{header.name ?? header.id}</HeaderCell>
-					})}
-				</div>
+		<div className={clsx("border border-table-border rounded-md overflow-hidden grow self-start max-h-full flex flex-col", props.className)}>
+			<div className={"grid overflow-x-auto overflow-y-auto"} style={{gridTemplateColumns: size.join(" ")}}>
+				{/*<div className={"contents sticky top-0"}>*/}
+				{props.header.map((header, index) => {
+					const sortOrder = header.sortable !== false ? props.sort?.find(v => v.id === header.id)?.order ?? SortOrder.None : SortOrder.None;
+					const setSort = props.setSort && ((sort: SortOrder) => {
+						if(props.setSort === undefined) return;
+						props.setSort(prev => {
+							const index = prev.findIndex(v => v.id === header.id);
+							if(index !== -1) {
+								prev[index].order = sort;
+							} else {
+								prev.push({id: header.id, order: sort});
+							}
+							return [...prev];
+						})
+					});
+					return <HeaderCell key={index} sortable={header.sortable ?? true} sort={sortOrder} setSort={setSort}>{header.name ?? header.id}</HeaderCell>
+				})}
+				{/*</div>*/}
 				{props.error !== undefined && <div className={"col-span-full h-24 flex items-center justify-center"}>{props.error}</div> || props.loading && (props.loadingElement ?? <DefaultLoading/>) || props.data.map((row, index) => {
 					return (
 						<div className={"contents group"} key={index}>
